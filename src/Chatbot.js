c4e90565d064d986a2792ef6b0b148e5175d970b
Adeola David Adelakun <aadelakun28@gmail.com>
2023-05-28 22:52:13 +0100

    UI Revamp

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -1,98 +1,92 @@
-import './App.css';
-import React, { Component } from 'react';
-import axios from "axios";
+import { useState } from "react";
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
+// import dotenv from "dotenv";
 
-class Chatbot extends Component {
+const configuration = new Configuration({
+  organization: "org-hDLwSg4mxhinjecFIxoGbxcP",
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      messages: [],
-    };
-  }
+function Chatbot() {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
 
-  componentDidMount() {
-    // Add initial message from bot to messages list
-    this.setState({
-      messages: [
-        {
-          sender: "bot",
-          message: "Welcome to StarTrek Bot! I answer questions based on the dataset of all StarTrek movies and TV show scripts. Here are a few things you can ask me:"
-        },
-        {
-          sender: "bot",
-          message: "-> What is the name of the ship in Star Trek?"
-        },
-        {
-          sender: "bot",
-          message: "-> Who is the captain of the USS Enterprise?"
-        }
-      ]
-    });
-  }
+  const chat = async (e, message) => {
+    e.preventDefault();
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const userMessage = event.target.elements.userInput.value;
-    // Set messages state
-    this.setState({ messages: [...this.state.messages, { type: 'user', message: userMessage }] });
-    event.target.elements.userInput.value = '';
-    this.sendMessage(userMessage);
-  }
+    if (!message) return;
+    setIsTyping(true);
+    window.scrollTo(0, 100);
 
-  sendMessage = async (userMessage) => {
-    const openaiEndpoint = 'https://api.openai.com/v1/chat/completions';
-    const openaiApiKey = process.env.REACT_APP_OPENAI_API_KEY;
-    const model = 'gpt-3.5-turbo';
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
 
-    const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${openaiApiKey}`
-    };
+    setMessage("");
 
-    const data = {
-      'model': model,
-      'messages': [{ "role": "user", "content": userMessage }]
-    };
-
-    // Make a POST request to the OpenAI GPT API
-    axios.post(openaiEndpoint, data, { headers })
-      .then(response => {
-        const chatResponse = response.data.choices[0].message.content;
-        this.setState({
-          messages: [...this.state.messages, {
-            sender: "bot",
-            message: chatResponse
-          }]
-        });
-        console.log(chatResponse);
-        // Do something with the chat response
+    await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "It's so interesting to interract here",
+          },
+          ...chats,
+        ],
       })
-      .catch(error => {
-        console.error(error);
-        // Handle the error
+      .then((res) => {
+        msgs.push(res.data.choices[0].message);
+        setChats(msgs);
+        setIsTyping(false);
+        window.scrollTo(0, 100);
+      })
+      .catch((error) => {
+        console.log(error);
       });
   };
 
+  return (
+    <main>
+      <h1>Chatbot-by-Ade</h1>
 
-  render() {
-    return (
-      <div className="chatbot">
-        <div className="chat-window">
-          {this.state.messages.map((message, index) => (
-            <div key={index} className={`message ${message.type}`}>
-              {message.message}
-            </div>
-          ))}
-        </div>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="userInput" placeholder="Type your message here" />
-          <button type="submit">Send</button>
-        </form>
+      <section>
+        {chats && chats.length
+          ? chats.map((chat, index) => (
+              <p
+                key={index}
+                className={chat.role === "user" ? "user_msg" : ""}
+              >
+                <span>
+                  <b>{chat.role.toUpperCase()}</b>
+                </span>
+                <span>:</span>
+                <span>{chat.content}</span>
+              </p>
+            ))
+          : ""}
+      </section>
+
+      <div className={isTyping ? "" : "hide"}>
+        <p>
+          <i>{isTyping ? "Typing" : ""}</i>
+        </p>
       </div>
-    );
-  }
 
+      <form action="" onSubmit={(e) => chat(e, message)}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Type in your messages here and click Enter..."
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </main>
+  );
 }
 
 export default Chatbot;
